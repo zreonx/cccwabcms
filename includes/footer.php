b78e06be0a24c00c014533954e63235ee4c18135
Zreon <56314835+zreonx@users.noreply.github.com>
2023-01-22 12:54:00 +0800

    Index page / master page

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+        
+        </div>
+    <div class="footer mt-auto text-center p-3 text-light">Copyright &copy; <?php echo date("Y"); ?>. All rights reserve. Designed By <strong><?php echo 'Zorenl Rkyle Dayrit' ?></strong><a href="#"></a></div>
+</body>
+</html>
